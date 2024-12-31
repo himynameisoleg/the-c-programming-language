ae688a4d45003cb6487448eab8bae894ebde274f
Oleg <heyitsoleg@gmail.com>
2024-12-30 22:10:24 -0600

    symbilic constants

--- ch01/hello.c
+++ ch01/hello.c
@@ -20,12 +20,13 @@
 // variation with `for`
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main(void) {
   int fahr;
 
-  for (fahr = 0; fahr <= 300; fahr = fahr + 20)
-    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
-
-  for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+  for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
     printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 }
