447c3e9290de4b2a864a5e8bac24eb59d30f1c76
Oleg <heyitsoleg@gmail.com>
2024-12-31 13:19:38 -0600

    copying input to output

--- ch01/hello.c
+++ ch01/hello.c
@@ -18,15 +18,28 @@
 // }
 
 // variation with `for`
+// #include <stdio.h>
+//
+// #define LOWER 0
+// #define UPPER 300
+// #define STEP 20
+//
+// int main(void) {
+//   int fahr;
+//
+//   for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+//     printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+// }
+
+// copy input to output
 #include <stdio.h>
 
-#define LOWER 0
-#define UPPER 300
-#define STEP 20
-
 int main(void) {
-  int fahr;
+  int c;
 
-  for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+  c = getchar();
+  while (c != EOF) {
+    putchar(c);
+    c = getchar();
+  }
 }
