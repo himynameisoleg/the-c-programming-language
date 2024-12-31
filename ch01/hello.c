2337fcf87a1be1c5b78c8c332dd1dd48aa5b40e5
Oleg <heyitsoleg@gmail.com>
2024-12-30 21:53:50 -0600

    for loops

--- ch01/hello.c
+++ ch01/hello.c
@@ -1,18 +1,31 @@
+// #include <stdio.h>
+//
+// int main() {
+//   float fahr, celsius;
+//   float lower, upper, step;
+//
+//   lower = 0;
+//   upper = 300;
+//   step = 20;
+//
+//   fahr = lower;
+//
+//   while (fahr <= upper) {
+//     celsius = (5.0 / 9.0) * (fahr - 32.0);
+//     printf("%3.0f %6.1f\n", fahr, celsius);
+//     fahr = fahr + step;
+//   }
+// }
+
+// variation with `for`
 #include <stdio.h>
 
-int main() {
-  int fahr, celsius;
-  int lower, upper, step;
+int main(void) {
+  int fahr;
 
-  lower = 0;
-  upper = 300;
-  step = 20;
+  for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 
-  fahr = lower;
-
-  while (fahr <= upper) {
-    celsius = 5 * (fahr - 32) / 9;
-    printf("%3d %6d\n", fahr, celsius);
-    fahr = fahr + step;
-  }
+  for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 }
