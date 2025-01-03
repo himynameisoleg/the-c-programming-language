61485cef0c432384de947960aa4bda368e855c01
Oleg <heyitsoleg@gmail.com>
2025-01-02 22:03:20 -0600

    ch2 exercise 2-1

---
+++ ch02/hello.c
@@ -0,0 +1,19 @@
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+
+int main(void) {
+  printf("%d\n", CHAR_MIN);
+  printf("%d\n", CHAR_MAX);
+
+  printf("%d\n", SHRT_MIN);
+  printf("%d\n", SHRT_MAX);
+
+  printf("%d\n", INT_MAX);
+  printf("%d\n", INT_MIN);
+
+  printf("%ld\n", LONG_MAX);
+  printf("%ld\n", LONG_MAX);
+
+  return 1;
+}
