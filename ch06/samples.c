a139bc635112a1565c6d09cfce8cbcb1a0760229
Oleg <heyitsoleg@gmail.com>
2025-01-14 08:25:41 -0600

    structures and functions

--- ch06/samples.c
+++ ch06/samples.c
@@ -6,8 +6,32 @@ struct point {
   int y;
 };
 
+struct rect {
+  struct point pt1;
+  struct point pt2;
+};
+
+struct point makepoint(int, int);
+
+struct point makepoint(int x, int y) {
+  struct point temp;
+
+  temp.x = x;
+  temp.y = y;
+  return temp;
+}
+
 int main(int argc, char *argv[]) {
-  struct point pt = {20, 10};
-  printf("%d, %d\n", pt.x, pt.y);
+
+  struct rect screen;
+  struct point middle;
+
+  screen.pt1 = makepoint(0, 0);
+  screen.pt2 = makepoint(300, 100);
+
+  middle = makepoint((screen.pt1.x + screen.pt2.x) / 2,
+                     (screen.pt1.y + screen.pt2.y) / 2);
+
+  printf("%d, %d\n", middle.x, middle.y);
   return 0;
 }
